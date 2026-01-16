29651ceb1ddd26fed22b61471f2425a9f56337c1
ByteFlow <fakeshadow1337@gmail.com>
2026-01-16 16:38:54 +0800

    Time: 46 ms (90.61%), Space: 22.4 MB (19.74%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -8,5 +8,5 @@ class Solution:
                 if matrix[i][j] == 1:
                     if  i >0 and j >0:
                         matrix[i][j] = min(matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1]) +1
-                total += matrix[i][j]
+                    total += matrix[i][j]
         return total
