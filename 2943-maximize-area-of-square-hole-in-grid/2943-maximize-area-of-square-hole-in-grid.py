3e6dbf65e0bd07635ed03c0d726f664782241b4f
ByteFlow <fakeshadow1337@gmail.com>
2026-01-16 16:50:22 +0800

    Time: 3 ms (50.69%), Space: 19.4 MB (17.81%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.py
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.py
@@ -1,18 +1,15 @@
 class Solution:
     def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
         def get_maxgrid(bars):
-            if not bars:
-                return 1
-            else:
-                bars.sort()
-                max_len=1
-                current_len=1
-                for i in range(len(bars)):
-                    if bars[i] == bars[i-1]+1:
-                        current_len+=1
-                    else:
-                        current_len=1
-                    max_len=max(max_len,current_len) 
+            bars.sort()
+            max_len=1
+            current_len=1
+            for i in range(1,len(bars)):
+                if bars[i] == bars[i-1]+1:
+                    current_len+=1
+                else:
+                    current_len=1
+                max_len=max(max_len,current_len) 
             return max_len+1
         hgrid = get_maxgrid(hBars)
         vgrid = get_maxgrid(vBars)
