520d5b716469dea461393e0c445ae8fc55719957
ByteFlow <fakeshadow1337@gmail.com>
2026-01-18 23:54:17 +0800

    Time: 0 ms (100%), Space: 20.5 MB (5.41%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -1,8 +1,8 @@
 class Solution:
-    def repeatedNTimes(self, nums: List[int]) -> int:
-        exist = set()
-        for i in range(len(nums)):
-            if nums[i] in exist:
-                return nums[i]
-            else:
-                exist.add(nums[i])
+        def repeatedNTimes(self, nums: List[int]) -> int:
+            exist = set()
+            for i in range(len(nums)):
+                if nums[i] in exist:
+                    return nums[i]
+                else:
+                    exist.add(nums[i])
