4bf73762cbc0b9b8c9cbc26705e7c10e10ed141b
ByteFlow <fakeshadow1337@gmail.com>
2026-03-18 14:45:31 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,19 +2,14 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1 or len(s) <= numRows:
             return s
-        rows = [""] * numRows
         step = 1
         current_row = 0
+        rows = [""] * numRows
         for char in s:
             rows[current_row] += char
-            if current_row == numRows - 1:
-                step = -1
-            elif current_row == 0:
+            if current_row == 0:
                 step = 1
+            elif current_row == numRows - 1:
+                step = -1
             current_row += step
         return "".join(rows)
-
-        
-            
-                
-        
